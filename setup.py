fba7390e7799a99e6c8f1d4fa21006cf4047e935
sonicpierre <pvirgaux@gmail.com>
2022-04-22 18:32:32 +0200

    Modification partage librairie

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='model_creator',
+    name='model_creator_bird_sing',
     packages=find_packages(),
     version='0.1.0',
     description='Autoencoder_singing',
     author='VIRGAUX Pierre',
     license='MIT',
-    install_requires=['tensorflow==2.3.1','mutagen','pydub','ray'],
+    install_requires=['tensorflow==2.3.1','mutagen','pydub','ray', 'librosa'],
 )
