a2b1adb87a83d9e0d5379be582b91e8798af878a
sonicpierre <pvirgaux@gmail.com>
2022-04-23 12:27:25 +0200

    Ajout lancement final

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import find_packages, setup
 setup(
     name='model_creator_bird_sing',
     packages=find_packages(),
-    version='0.1.0',
-    description='Autoencoder_singing',
+    version='0.1.1',
+    description='Autoencoder singing',
     author='VIRGAUX Pierre',
     license='MIT',
-    install_requires=['tensorflow==2.3.1','mutagen','pydub','ray', 'librosa'],
+    install_requires=['tensorflow==2.3.1','mutagen','pydub','ray', 'librosa', 'pickle'],
 )
